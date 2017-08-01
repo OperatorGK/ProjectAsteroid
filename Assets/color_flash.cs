09127f8fc81321497c2c49ee8e283fb430289eea
OperatorGK <german_kalinichenko@mail.ru>
2017-08-01 15:37:27 +0300

    Test commit #1

---
+++ Assets/color_flash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_flash : MonoBehaviour {
+    public float duration = 1.0F;
+    public Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        float hue = Mathf.PingPong(Time.time, duration) / duration;
+        rend.material.color = Color.HSVToRGB(hue, 0.8f, 0.8f);
+    }
+}
