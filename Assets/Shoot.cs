f4b2e5d57ffe13d87f01cecc30082b14c55620e0
PenguinKiller <wkololowka2014@yandex.com>
2017-10-18 20:52:49 +0300

    сделал критерий попаданий

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32a7e4c19f06790320a6d70279e072c0a18790631a7486b1a0efbb682f41a1c9
-size 434
+oid sha256:351c7ce10f2389d2ae244094fc53e5d5137fadf76915c7eed608b697bf7078c8
+size 649
