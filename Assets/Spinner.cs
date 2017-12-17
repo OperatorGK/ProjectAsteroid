e3637dd510d293171211f08a88462ffef3277a47
PenguinKiller <wkololowka2014@yandex.com>
2017-12-17 18:54:43 +0300

    Walls

--- Assets/Spinner.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f103013cbc67179e972b149ce2223752b18a9b8902f1e5545b226135c84dd4c8
-size 276
---
+++ Assets/Transper.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f819b8d4ac314a6aa1cac26b07b3b16f59a9f60388c427fa864df959659556fe
+size 963
