24306e4421020fa81fc6f5376d5a1412f1b216ab
Kostya <wkololowka2014@yandex.com>
2018-02-14 22:24:25 +0300

    bullet indicator

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08579bbf003b1d3a6e223200e6ecb4e550f428525a4d349138b2d10b91e62947
-size 866
+oid sha256:47543e77dcb988b7e93ad6896fc56c92572cc248f1c85a2ff950e150fd457a46
+size 1387
