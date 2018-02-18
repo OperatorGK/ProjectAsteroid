dfca1b8162af4b10abc66e7edc8d60ec09d6c30a
Kostya <wkololowka2014@yandex.com>
2018-02-18 16:59:14 +0300

    hpbar

---
+++ Assets/Health.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f4515a9b5c601bb85450ea40df9ec48970dfc049d07ba5917222159c59af1e9
+size 362
--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47543e77dcb988b7e93ad6896fc56c92572cc248f1c85a2ff950e150fd457a46
-size 1387
+oid sha256:0dca92699b58e0da639cc9dabfece0888a91191fed6168b0be8a9d50797d59ae
+size 1390
